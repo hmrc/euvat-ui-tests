b6f0b41d01fdd5f2f98a7920904e9f1c1178d8da
sultanwasimraj <258210018+sultanwasimraj@users.noreply.github.com>
2026-05-18 13:55:41 +0100

    Update the scripts for Second Business Activity Code - DTR-5134

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/Add2ndBusinessActivityCode.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages
+
+import org.openqa.selenium.{By, Keys}
+
+object Add2ndBusinessActivityCode extends BasePage {
+
+  override def pageUrl: String = "business-activity-code-two"
+
+  override def pageTitle: String =
+    "2nd business activity for this claim - EU VAT - GOV.UK"
+
+  val secondBusinessActivityCodeDropdown = "#value-input__listbox"
+  val txtSecondBusinessActivityCode: By  = By.cssSelector("#value-input")
+
+  def selectSecondBusinessActivityCode(businessActivityCode: String): Unit = {
+    clearSecondBusinessActivityCode(txtSecondBusinessActivityCode)
+    input(txtSecondBusinessActivityCode, businessActivityCode)
+    clickSecondBusinessActivityCodeDropdown()
+    saveAndContinue()
+  }
+
+  def clickSecondBusinessActivityCodeDropdown(): Unit = click(By.cssSelector(secondBusinessActivityCodeDropdown))
+
+  /** Clear already selected country value */
+  def clearSecondBusinessActivityCodeDropdown(selector: By): Unit = {
+    val element = waitForVisibilityOfElement(selector)
+    element.sendKeys(Keys.CONTROL, "a")
+    element.sendKeys(Keys.DELETE)
+  }
+
+  def clearSecondBusinessActivityCode(selector: By): Unit = {
+    val el      = waitForVisibilityOfElement(selector)
+    el.click()
+    val current = Option(el.getAttribute("value")).getOrElse("")
+    if (current.nonEmpty) {
+      for (_ <- 1 to current.length)
+        el.sendKeys(Keys.BACK_SPACE)
+    }
+  }
+
+}
