94acce18ed18c157c260a71c2a69832dbddafe85
WasimRaj <258210018+sultanwasimraj@users.noreply.github.com>
2026-06-18 15:06:59 +0100

    Updating the scripts as part of DTR-5974 which is content changes

--- src/test/scala/uk/gov/hmrc/ui/pages/BusinessActivityTwo.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/BusinessActivityTwo.scala
@@ -20,7 +20,7 @@ object BusinessActivityTwo extends BasePage {
 
   override def pageUrl: String = "business-activity-2"
 
-  override def pageTitle: String = "Business activities for this claim - EU VAT - GOV.UK"
+  override def pageTitle: String = "Business activity for this claim - EU VAT - GOV.UK"
 
   val lnkChangeBusinessActivityTwo =
     "#main-content > div > div > form > dl > div > dd.govuk-summary-list__actions > ul > li:nth-child(1) > a"
