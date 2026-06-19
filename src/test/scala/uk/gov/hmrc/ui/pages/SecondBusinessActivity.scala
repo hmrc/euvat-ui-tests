7ed38e9b60a36b01200f146154af2ef58274fdcf
Srikanth Madduri <srikanth.madduri@digital.hmrc.gov.uk>
2026-06-19 13:11:49 +0100

    clean up code

--- src/test/scala/uk/gov/hmrc/ui/pages/SecondBusinessActivity.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/SecondBusinessActivity.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.ui.pages
 
 import org.openqa.selenium.{By, Keys}
 
-object SeoncdBusinessActivity extends BasePage {
+object SecondBusinessActivity extends BasePage {
 
   override def pageUrl: String = "what-is-the-second-SIC-code"
 
@@ -26,19 +26,10 @@ object SeoncdBusinessActivity extends BasePage {
 
   val txtSecondBusinessActivityCode: By = By.cssSelector("#value")
 
-//  def selectSecondBusinessActivityCode(businessActivityCode: String): Unit = {
-//    clearSecondBusinessActivityCode(txtSecondBusinessActivityCode)
-//    input(txtSecondBusinessActivityCode, businessActivityCode)
-////    clickSecondBusinessActivityCodeDropdown()
-//    continue()
-//  }
-
   def enterSecondBusinessActivityCode(businessActivityCode: String): Unit = {
     input(txtSecondBusinessActivityCode, businessActivityCode)
     continue()
   }
-
-//  def clickSecondBusinessActivityCodeDropdown(): Unit = click(By.cssSelector(secondBusinessActivityCodeDropdown))
 
   /** Clear already selected country value */
   def clearSecondBusinessActivityCodeDropdown(selector: By): Unit = {
