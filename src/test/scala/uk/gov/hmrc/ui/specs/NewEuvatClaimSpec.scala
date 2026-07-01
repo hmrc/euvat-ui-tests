23b0c8617d7b820b2bf70a2d2fde633e7aa88dd3
Srikanth Madduri <srikanth.madduri@digital.hmrc.gov.uk>
2026-07-01 12:30:09 +0100

    DTR-4294 Add tests for total vat claim page

--- src/test/scala/uk/gov/hmrc/ui/specs/NewEuvatClaimSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/NewEuvatClaimSpec.scala
@@ -172,6 +172,9 @@ class NewEuvatClaimSpec
 //      TotalVatPaid.verifyPageTitle(TotalVatPaid.pageTitle)
 //      TotalVatPaid.submitTotalVatPaid("100")
 
+//      TotalVatClaim.verifyPageTitle(TotalVatPaid.pageTitle)
+//      TotalVatClaim.submitTotalVatClaim("100")
+
       MakeEuvatClaim.signOut()
     }
   }
