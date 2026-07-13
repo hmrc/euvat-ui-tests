c7c049f296c5279c56d335454b4b95f3598b194f
JohnWhitfield <22446460+JohnWhitfield@users.noreply.github.com>
2026-07-13 08:42:55 +0100

    Uncommented purchase journey. Corrected page titles. Added RandomUtils to generate credId.

--- src/test/scala/uk/gov/hmrc/ui/utils/MongoHelper.scala
+++ src/test/scala/uk/gov/hmrc/ui/utils/MongoHelper.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.ui.util
+package uk.gov.hmrc.ui.utils
 
 import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase, SingleObservableFuture}
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Suite}
