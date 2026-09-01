934d22acbb3c5b9d254c26aabc9df0282e023c54
JohnWhitfield <22446460+JohnWhitfield@users.noreply.github.com>
2026-09-01 15:13:37 +0100

    Dependancies updated and formatted after merge with main

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,14 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "ui-test-runner"     % "0.55.0" % Test,
-    "org.mongodb.scala"       %% "mongo-scala-driver" % "5.11.0" cross CrossVersion.for3Use2_13,
-    "com.oracle.database.jdbc" % "ojdbc8"             % "23.26.3.0.0", // Oracle JDBC driver
-    "org.scalatest"           %% "scalatest"          % "3.2.20" % Test,
-    "org.seleniumhq.selenium"  % "selenium-java"      % "4.48.0" % Test,
-    "org.apache.poi"           % "poi-ooxml"          % "5.5.1"  % Test,
+    "uk.gov.hmrc" %% "ui-test-runner" % "0.55.0" % Test,
     "com.typesafe.play" %% "play-ahc-ws-standalone" % "2.2.17" % Test,
-    ("com.typesafe.play" %% "play-json" % "2.10.8" % Test).cross(CrossVersion.for3Use2_13)
+    ("com.typesafe.play" %% "play-json" % "2.10.8" % Test).cross(CrossVersion.for3Use2_13),
+    "org.mongodb.scala" %% "mongo-scala-driver" % "5.11.0" cross CrossVersion.for3Use2_13,
+    "com.oracle.database.jdbc" % "ojdbc8" % "23.26.3.0.0", // Oracle JDBC driver
+    "org.scalatest" %% "scalatest" % "3.2.20" % Test,
+    "org.seleniumhq.selenium" % "selenium-java" % "4.48.0" % Test,
+    "org.apache.poi" % "poi-ooxml" % "5.5.1" % Test
   )
 
 }
