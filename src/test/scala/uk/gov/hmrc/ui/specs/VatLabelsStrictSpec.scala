6f51f3f9ffd0fc040dc459f331be658eb0fb9312
JohnWhitfield <22446460+JohnWhitfield@users.noreply.github.com>
2026-09-04 07:14:48 +0100

    Reverting spec to 1 scenario per mapping. Adding report file and withClue logging

--- src/test/scala/uk/gov/hmrc/ui/specs/VatLabelsStrictSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/VatLabelsStrictSpec.scala
@@ -16,17 +16,31 @@
 
 package uk.gov.hmrc.ui.specs
 
-import org.scalatest.BeforeAndAfterEach
-import org.scalatest.GivenWhenThen
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, GivenWhenThen}
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.verbs.ShouldVerb
 import uk.gov.hmrc.selenium.webdriver.{Browser, ScreenshotOnFailure}
 import uk.gov.hmrc.ui.pages.{AuthorityWizard, ClaimAnEUVATRefund}
 import uk.gov.hmrc.ui.pages.claim.*
 import uk.gov.hmrc.ui.pages.purchase.*
-import uk.gov.hmrc.ui.tags.Local
-import uk.gov.hmrc.ui.utils.CountryCodeMappingReader
 import uk.gov.hmrc.ui.utils.PurchaseFlowRouter
+import uk.gov.hmrc.ui.tags.Local
+import uk.gov.hmrc.ui.utils.{CountryCodeMappingReader, MappingRow}
+
+import java.io.{File, PrintWriter}
+import scala.collection.mutable.ListBuffer
+
+final case class MappingFailure(
+  countryCode: String,
+  countryName: String,
+  code: String,
+  subCode: Option[String],
+  expectedPage: String,
+  actualPage: String,
+  expectedLabels: Seq[String],
+  actualLabels: Seq[String],
+  message: String
+)
 
 class VatLabelsStrictSpec
     extends AnyFeatureSpec
@@ -34,14 +48,18 @@ class VatLabelsStrictSpec
     with GivenWhenThen
     with ShouldVerb
     with BeforeAndAfterEach
+    with BeforeAndAfterAll
     with Browser
     with ScreenshotOnFailure {
 
-  private val rows = CountryCodeMappingReader.loadFromResource()
+  private val rows     = CountryCodeMappingReader.loadFromResource()
+  private val failures = ListBuffer.empty[MappingFailure]
+
+  private def topLevelRows(countryCode: String, code: String): Seq[MappingRow] =
+    rows.filter(r => r.countryCode == countryCode && r.code == code)
 
   private def expectedSubCodeLabels(countryCode: String, code: String): Seq[String] =
-    rows
-      .filter(r => r.countryCode == countryCode && r.code == code)
+    topLevelRows(countryCode, code)
       .flatMap(_.subCodeLabel)
       .distinct
 
@@ -90,29 +108,132 @@ class VatLabelsStrictSpec
     PurchaseType.verifyPageTitle(PurchaseType.pageTitle)
   }
 
+  private def recordFailure(
+    countryCode: String,
+    countryName: String,
+    code: String,
+    subCode: Option[String],
+    expectedPage: String,
+    actualPage: String,
+    expectedLabels: Seq[String],
+    actualLabels: Seq[String],
+    message: String
+  ): Unit =
+    failures += MappingFailure(
+      countryCode = countryCode,
+      countryName = countryName,
+      code = code,
+      subCode = subCode,
+      expectedPage = expectedPage,
+      actualPage = actualPage,
+      expectedLabels = expectedLabels.sorted,
+      actualLabels = actualLabels.sorted,
+      message = message
+    )
+
+  private def writeCsvReport(): Unit = {
+    val dir = new File("target")
+    if (!dir.exists()) dir.mkdirs()
+
+    val file = new File(dir, "vat-label-mapping-failures.csv")
+    val pw   = new PrintWriter(file)
+
+    try {
+      pw.println("countryCode,countryName,code,subCode,expectedPage,actualPage,message,expectedLabels,actualLabels")
+      failures.foreach { f =>
+        def esc(s: String): String = "\"" + s.replace("\"", "\"\"") + "\""
+
+        pw.println(
+          Seq(
+            esc(f.countryCode),
+            esc(f.countryName),
+            esc(f.code),
+            esc(f.subCode.getOrElse("")),
+            esc(f.expectedPage),
+            esc(f.actualPage),
+            esc(f.message),
+            esc(f.expectedLabels.mkString(" | ")),
+            esc(f.actualLabels.mkString(" | "))
+          ).mkString(",")
+        )
+      }
+    } finally pw.close()
+  }
+
+  override def afterAll(): Unit =
+    try
+      if (failures.nonEmpty) {
+        println("\n================ VAT LABEL MAPPING FAILURE SUMMARY ================")
+        failures.foreach { f =>
+          println(
+            s"""
+               |countryCode=${f.countryCode}
+               |countryName=${f.countryName}
+               |code=${f.code}
+               |subCode=${f.subCode.getOrElse("-")}
+               |message=${f.message}
+               |expectedPage=${f.expectedPage}
+               |actualPage=${f.actualPage}
+               |expectedLabels=${f.expectedLabels.mkString("[", ", ", "]")}
+               |actualLabels=${f.actualLabels.mkString("[", ", ", "]")}
+               |----------------------------------------------------------------
+               |""".stripMargin
+          )
+        }
+        writeCsvReport()
+      }
+    finally
+      super.afterAll()
+
   Feature("Purchase mapping labels from CountryCodeMapping.xlsx") {
 
     rows.map(r => (r.countryCode, r.country)).distinct.foreach { case (countryCode, countryName) =>
-      Scenario(s"Validate sub code labels for $countryCode", Local) {
-        Given(s"I navigate quickly to PurchaseType for $countryName")
-        navigateToPurchaseType(countryName)
+      Seq("1", "3", "7", "9", "10").foreach { code =>
+        val subLabels = expectedSubCodeLabels(countryCode, code)
 
-        val codesForCountry = rows.filter(_.countryCode == countryCode).map(_.code).distinct.sorted
+        if (subLabels.nonEmpty) {
+          Scenario(s"Validate sub code labels for country=$countryCode code=$code", Local) {
+            println(s"[DEBUG] Starting scenario: country=$countryCode countryName=$countryName code=$code subCode=-")
 
-        codesForCountry.foreach { code =>
-          val expectedLabels = expectedSubCodeLabels(countryCode, code)
+            Given(s"I navigate to PurchaseType for $countryName")
+            navigateToPurchaseType(countryName)
 
-          if (expectedLabels.nonEmpty) {
-            When(s"I select purchase type code $code for $countryCode")
+            When(s"I select purchase type code $code")
             PurchaseType.selectPurchaseType(PurchaseFlowRouter.purchaseTypeLabelFor(code))
 
-            Then(s"I should see the expected sub code labels for $countryCode / $code")
-            val page = PurchaseFlowRouter.topLevelPageFor(code)
-            page.assertCurrentPage()
-            page.assertLabelsContain(expectedLabels)
+            Then(s"I should see the expected sub code labels")
+            val page         = PurchaseFlowRouter.topLevelPageFor(code)
+            val actualLabels = page.availableLabels()
 
-            PurchaseType.navigateBack()
-            PurchaseType.verifyPageTitle(PurchaseType.pageTitle)
+            try
+              withClue(
+                s"""
+                   |Sub-code label mismatch
+                   |country=$countryCode
+                   |countryName=$countryName
+                   |code=$code
+                   |expected=${subLabels.sorted.mkString("[", ", ", "]")}
+                   |actual=${actualLabels.sorted.mkString("[", ", ", "]")}
+                   |""".stripMargin
+              ) {
+                page.assertCurrentPage()
+                page.assertLabelsContain(subLabels)
+              }
+            catch {
+              case e: Throwable =>
+                recordFailure(
+                  countryCode = countryCode,
+                  countryName = countryName,
+                  code = code,
+                  subCode = None,
+                  expectedPage = page.pageTitle,
+                  actualPage = page.getPageTitle,
+                  expectedLabels = subLabels,
+                  actualLabels = actualLabels,
+                  message = e.getMessage
+                )
+                throw e
+            }
           }
         }
       }
@@ -125,25 +246,74 @@ class VatLabelsStrictSpec
         val expectedLabels = expectedSubCategoryLabels(countryCode, code, subCode)
 
         if (expectedLabels.nonEmpty) {
-          Scenario(s"Validate sub category labels for $countryCode code=$code subCode=$subCode", Local) {
-            Given(s"I navigate quickly to PurchaseType for $countryName")
+          Scenario(s"Validate sub category labels for country=$countryCode code=$code subCode=$subCode", Local) {
+            println(
+              s"[DEBUG] Starting scenario: country=$countryCode countryName=$countryName code=$code subCode=$subCode"
+            )
+
+            Given(s"I navigate to PurchaseType for $countryName")
             navigateToPurchaseType(countryName)
 
-            When(s"I select purchase type $code")
+            When(s"I select purchase type code $code")
             PurchaseType.selectPurchaseType(PurchaseFlowRouter.purchaseTypeLabelFor(code))
 
-            val topPage = PurchaseFlowRouter.topLevelPageFor(code)
-            topPage.assertCurrentPage()
-
+            val topPage      = PurchaseFlowRouter.topLevelPageFor(code)
             val subCodeLabel = groupedRows.head.subCodeLabel.get
+
+            withClue(
+              s"""
+                 |Top-level page mismatch before sub-code selection
+                 |country=$countryCode
+                 |countryName=$countryName
+                 |code=$code
+                 |subCode=$subCode
+                 |expectedPage=${topPage.pageTitle}
+                 |actualPage=${topPage.getPageTitle}
+                 |""".stripMargin
+            ) {
+              topPage.assertCurrentPage()
+            }
 
             And(s"I select sub code $subCodeLabel")
             topPage.selectByVisibleLabel(subCodeLabel)
 
-            Then(s"I should see the expected sub category labels for $countryCode / $code / $subCode")
-            val subPage = PurchaseFlowRouter.subCategoryPageFor(code, subCode)
-            subPage.assertCurrentPage()
-            subPage.assertLabelsContain(expectedLabels)
+            Then(s"I should see the expected sub category labels")
+            val subPage      = PurchaseFlowRouter.subCategoryPageFor(code, subCode)
+            val actualLabels = subPage.availableLabels()
+
+            try
+              withClue(
+                s"""
+                   |Sub-category label mismatch
+                   |country=$countryCode
+                   |countryName=$countryName
+                   |code=$code
+                   |subCode=$subCode
+                   |subCodeLabel=$subCodeLabel
+                   |expected=${expectedLabels.sorted.mkString("[", ", ", "]")}
+                   |actual=${actualLabels.sorted.mkString("[", ", ", "]")}
+                   |expectedPage=${subPage.pageTitle}
+                   |actualPage=${subPage.getPageTitle}
+                   |""".stripMargin
+              ) {
+                subPage.assertCurrentPage()
+                subPage.assertLabelsContain(expectedLabels)
+              }
+            catch {
+              case e: Throwable =>
+                recordFailure(
+                  countryCode = countryCode,
+                  countryName = countryName,
+                  code = code,
+                  subCode = Some(subCode),
+                  expectedPage = subPage.pageTitle,
+                  actualPage = subPage.getPageTitle,
+                  expectedLabels = expectedLabels,
+                  actualLabels = actualLabels,
+                  message = e.getMessage
+                )
+                throw e
+            }
           }
         }
       }
