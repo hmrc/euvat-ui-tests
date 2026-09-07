d3e6429132bd22b90ea6c74ec1b7c66bbb4dc44d
JohnWhitfield <22446460+JohnWhitfield@users.noreply.github.com>
2026-09-07 07:53:32 +0100

    Updated shell script for name change to spec

--- run_local_browser_vat.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-BROWSER=$1
-ENVIRONMENT=$2
-sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean compile -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dbrowser.option.headless=true "testOnly uk.gov.hmrc.ui.specs.VatLabelsStrictSpec" testReport
