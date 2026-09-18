e6b3ba96375a7f9c991c7922c9b48ed1d093c002
JohnWhitfield <22446460+JohnWhitfield@users.noreply.github.com>
2026-09-18 15:26:39 +0100

    Added scenario and pages for Germany

--- src/test/scala/uk/gov/hmrc/ui/pages/imports/ImportTypeOther.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/imports/ImportTypeOther.scala
@@ -18,20 +18,20 @@ package uk.gov.hmrc.ui.pages.imports
 
 import uk.gov.hmrc.ui.pages.BasePage
 
-object ImportTypeFood extends BasePage {
+object ImportTypeOther extends BasePage {
 
-  override def pageUrl: String = "import-food-drink-restaurant-cost"
+  override def pageUrl: String = "import-type-other"
 
-  override def pageTitle: String = "What is the type of food, drink or restaurant cost? - EU VAT - GOV.UK"
+  override def pageTitle: String = "What other category best describes the item? - EU VAT - GOV.UK"
 
-  val rdoFood = "#value_0"
-  val rdoNone = "#value_1"
+  val rdoPropertyRelated = "#value_0"
+  val rdoNone            = "#value_1"
 
-  def selectImportFoodType(radio: String): this.type = {
+  def selectImportTypeOther(radio: String): this.type = {
     val selector = radio match {
-      case "Food and drink from hotels" => rdoFood
-      case "None"                       => rdoNone
-      case _                            => throw new IllegalArgumentException(s"Invalid option: $radio")
+      case "Property-related costs"            => rdoPropertyRelated
+      case "None of these - give more details" => rdoNone
+      case _                                   => throw new IllegalArgumentException(s"Invalid option: $radio")
     }
     radioButton(selector)
     continue()
